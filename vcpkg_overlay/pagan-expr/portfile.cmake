40368fcad3e56b740070914e46d621cd423adbc2
TanninOne <seppleviathan@gmx.de>
2024-09-17 19:14:15 +0200

    fixed export and pagan::expr import

--- vcpkg_overlay/pagan-expr/portfile.cmake
+++ vcpkg_overlay/pagan-expr/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO TanninOne/pagan.expr
-  REF 47612a33eab32f50d9c67b4d846dbadf145ea3ea
-  SHA512 de74fb3c9bb277ecf2d5f39f6278098b574a65b02c88ba6cf1709da205a5c0b2c783d6bdc6643038e5ece1ac510cd4acbedffde5ca8f7fc3f7a1d75e0cfa9f49
+  REF 9715ad2875211148cafb6b3b0e050c3e933d38ae
+  SHA512 26c58644fbfe13cc304646cef5854e2093efac99b2226082082556025394d2424e92f1364062e52b6de889d68bb8569e4bec012cf71d0f26a16f46a0355643ad
   HEAD_REF main
 )
 
@@ -11,6 +11,9 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 vcpkg_cmake_config_fixup()
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
